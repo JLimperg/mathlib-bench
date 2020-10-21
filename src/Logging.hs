20205e3473237a525b14b5e00c3ad575006e66af
Jannis Limperg <jannis@limperg.de>
2020-10-21 18:40:14 +0200

    Add logging

---
+++ src/Logging.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Logging
+( LogSeverity(..)
+, logMsg
+, logInfo
+, logError
+) where
+
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TL
+
+data LogSeverity
+  = LogError
+  | LogInfo
+
+logSeverityToPrefix :: LogSeverity -> TL.Text
+logSeverityToPrefix = \case
+  LogError -> "[ERROR]"
+  LogInfo -> "[INFO]"
+
+logMsg :: LogSeverity -> TL.Text -> IO ()
+logMsg severity msg =
+  TL.putStrLn $ logSeverityToPrefix severity <> " " <> msg
+
+logInfo :: TL.Text -> IO ()
+logInfo = logMsg LogInfo
+
+logError :: TL.Text -> IO ()
+logError = logMsg LogError
