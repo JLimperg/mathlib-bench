3dbab9be6ae6bd4b3dd850b9d2df922fef1248f1
Jannis Limperg <jannis@limperg.de>
2020-10-21 17:45:50 +0200

    Set _NUM_THREADS = 1, _MEM_LIMIT_MB = 7000

--- src/Config.hs
+++ src/Config.hs
@@ -15,10 +15,10 @@ _MATHLIB_GIT_URL :: String
 _MATHLIB_GIT_URL = "https://github.com/leanprover-community/mathlib"
 
 _LEANPKG :: FilePath
-_LEANPKG = "/home/mathlib-bench/.elan/bin/leanpkg" -- TODO change
+_LEANPKG = "/home/mathlib-bench/.elan/bin/leanpkg"
 
 _NUM_THREADS :: Int
-_NUM_THREADS = 2
+_NUM_THREADS = 1
 
 _MEM_LIMIT_MB :: Int
-_MEM_LIMIT_MB = 6000
+_MEM_LIMIT_MB = 7000
