a200dc08d1530402e37f4caac03a80cf0ac29cbd
Jannis Limperg <jannis@limperg.de>
2020-10-29 17:29:08 +0100

    install.sh: Add explicit sudos

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ bin_dir="$(stack --allow-different-user path --local-install-root)/bin"
 
 id -u mathlib-bench &> /dev/null
 if test $? -ne 0; then
-    useradd --system --create-home --user-group mathlib-bench
+    sudo useradd --system --create-home --user-group mathlib-bench
 fi
 
-install -o mathlib-bench -g mathlib-bench -m 700 "$bin_dir/mathlib-bench-backend"  /home/mathlib-bench/
-install -o mathlib-bench -g mathlib-bench -m 700 "$bin_dir/mathlib-bench-frontend" /home/mathlib-bench/
+sudo install -o mathlib-bench -g mathlib-bench -m 700 "$bin_dir/mathlib-bench-backend"  /home/mathlib-bench/
+sudo install -o mathlib-bench -g mathlib-bench -m 700 "$bin_dir/mathlib-bench-frontend" /home/mathlib-bench/
 
 
 if which systemctl &> /dev/null; then
-    install -o 0 -g 0 -m 600 "$systemd_dir/mathlib-bench-backend.service" /etc/systemd/system
-    install -o 0 -g 0 -m 600 "$systemd_dir/mathlib-bench-frontend.service" /etc/systemd/system
-    systemctl daemon-reload
+    sudo install -o 0 -g 0 -m 600 "$systemd_dir/mathlib-bench-backend.service" /etc/systemd/system
+    sudo install -o 0 -g 0 -m 600 "$systemd_dir/mathlib-bench-frontend.service" /etc/systemd/system
+    sudo systemctl daemon-reload
 fi
