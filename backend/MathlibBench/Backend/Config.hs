1ef7731571fe8b494b6932a243259f962c131db5
Jannis Limperg <jannis@limperg.de>
2020-10-29 17:36:12 +0100

    backend: Use 4 threads, 20G RAM

--- backend/MathlibBench/Backend/Config.hs
+++ backend/MathlibBench/Backend/Config.hs
@@ -4,7 +4,7 @@ _LEANPKG :: FilePath
 _LEANPKG = "/home/mathlib-bench/.elan/bin/leanpkg"
 
 _NUM_THREADS :: Int
-_NUM_THREADS = 1
+_NUM_THREADS = 4
 
 _MEM_LIMIT_MB :: Int
-_MEM_LIMIT_MB = 7000
+_MEM_LIMIT_MB = 20000
